d9fd925a16b23efbccfe09326bc9065fb303d882
Jacob Ashdown <jcbashdown@gmail.com>
2025-12-11 14:23:33 +0300

    Test pre-commit hooks

---
+++ test_hooks.py
@@ -0,0 +1,13 @@
+"""Test file to demonstrate pre-commit hooks."""
+
+
+def example_function():
+    """Function with formatting issues."""
+    x = 1 + 2
+    y = 3 + 4
+    return x + y
+
+
+if __name__ == "__main__":
+    result = example_function()
+    print(result)
